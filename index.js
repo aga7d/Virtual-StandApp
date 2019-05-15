2e3c6198e7a5f19dd8d3ea8d9345633b49b4dfba
Aga <agidabrow@gmail.com>
2019-05-15 15:52:59 +0200

    changes in index.js

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api", api);
-/*if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-*/
+
 const PORT = process.env.PORT || 8081;
 app.listen(PORT);
